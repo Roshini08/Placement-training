f9cde7390f2dfea4937327029e5a7387b4e5af2b
Roshini08 <151143452+Roshini08@users.noreply.github.com>
2024-07-28 18:40:18 +0530

    Create  String.c

---
+++ 27.07.24/String.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+   
+    char str1[100] = "Hello, ";
+    char str2[50] = "world!";
+    
+   
+    strcat(str1, str2);
+    
+    
+    printf("Concatenated String: %s\n", str1);
+    
+    return 0;
+}

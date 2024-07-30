62b5d96811b7cbf91702655822bb64be697df572
Roshini08 <151143452+Roshini08@users.noreply.github.com>
2024-07-30 15:43:42 +0530

    Create Array 5.c

---
+++ 30.07.24/Array 5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int sum = 0;
+
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    float average = (float)sum / n;
+    printf("Average of elements in the array is %.2f\n", average);
+
+    return 0;
+}
